2a2ba66f09270297383446e4e24f9e418cbbc6ff
unknown <abdallahaliahmed517@gmail.com>
2024-10-03 00:44:11 +0300

    RRR

--- src/components/users.vue
+++ src/components/users.vue
@@ -33,15 +33,14 @@
         users: []
     })
 
-    const loadUsers = () =>{
-        axios.get(`http://localhost:3004/users`)
-        .then(response=>{
+    const loadUsers = async() =>{
+        try{
+            const response = await axios.get(`http://localhost:3004/users`);
             data.users = response.data;
             data.loading = false;
-        })
-        .catch(error=>{
+        } catch(error){
             $toast.error('Sorry, Something went wrong')
-        })
+        }
     }
     onMounted(()=>{
         loadUsers();
