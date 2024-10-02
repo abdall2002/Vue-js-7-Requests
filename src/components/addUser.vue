fae72601e23289130c2640cd655365cba7614d73
unknown <abdallahaliahmed517@gmail.com>
2024-10-03 01:34:26 +0300

    RRR

--- src/components/addUser.vue
+++ src/components/addUser.vue
@@ -17,40 +17,43 @@
 
         <button 
             class="btn btn-primary" 
-            @click.prevent="submitForm"
+            @click.prevent="addUser()"
         >Submit</button>
     </div>
 </template>
 
 <script setup>
-    import { reactive, ref } from 'vue';
-    import axios from 'axios';
-    import { useToast } from 'vue-toast-notification';
+    import postUser from '@/composables/postUser';
+    import { reactive } from 'vue';
+    // import axios from 'axios';
+    // import { useToast } from 'vue-toast-notification';
 
-    const $toast = useToast();
-    const loading = ref(false);
+    // const $toast = useToast();
+    // const loading = ref(false);
     const user = reactive({
         name:'',
         lastname:''
     });
+    
+    const { loading, addUser } = postUser(user);
 
-    const submitForm = () => {
-        loading.value = true;   
-        axios({
-            method:'POST',
-            url:'http://localhost:3004/users',
-            data:user
-        })
-        .then(()=>{
-            $toast.success('Great !!!')
-        })
-        .catch(error =>{
-            $toast.error('Sorry, try again later')
-        })
-        .finally(()=>{
-            loading.value = false;
-        })
-    }
+    // const submitForm = () => {
+    //     loading.value = true;   
+    //     axios({
+    //         method:'POST',
+    //         url:'http://localhost:3004/users',
+    //         data:user
+    //     })
+    //     .then(()=>{
+    //         $toast.success('Great !!!')
+    //     })
+    //     .catch(error =>{
+    //         $toast.error('Sorry, try again later')
+    //     })
+    //     .finally(()=>{
+    //         loading.value = false;
+    //     })
+    // }
 
 
 
